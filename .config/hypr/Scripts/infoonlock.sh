cb79b8b304cee40dd3ec1a17f95b61025e794adf
madhavgupta2775 <guptamadhav2775@gmail.com>
2024-11-05 15:29:20 +0530

    added hypr configs

---
+++ .config/hypr/Scripts/infoonlock.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+song_info=$(playerctl metadata --format '{{title}}      {{artist}}')
+
+# Get the current battery percentage
+battery_percentage=$(cat /sys/class/power_supply/BAT0/capacity)
+
+# Get the battery status (Charging or Discharging)
+battery_status=$(cat /sys/class/power_supply/BAT0/status)
+
+# Define the battery icons for each 10% segment
+battery_icons=("󰂃" "󰁺" "󰁻" "󰁼" "󰁽" "󰁾" "󰁿" "󰂀" "󰂁" "󰁹")
+
+# Define the charging icon
+charging_icon="󰂄"
+
+# Calculate the index for the icon array
+icon_index=$((battery_percentage / 10))
+
+# Get the corresponding icon
+battery_icon=${battery_icons[icon_index]}
+
+# Check if the battery is charging
+if [ "$battery_status" = "Charging" ]; then
+	battery_icon="$charging_icon"
+fi
+
+# Output the battery percentage and icon
+# echo "$battery_percentage% $battery_icon"
+
+# echo "$song_info" 
+
+case "$1" in
+--song)
+    echo "$song_info"
+    ;;
+--bat)
+    echo "$battery_percentage% $battery_icon"
+esac
